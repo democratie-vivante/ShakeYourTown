e144654ac64182151536a7af79e592add839bd40
Maël Bras <mael.bras.ext@idnow.io>
2026-03-03 15:15:41 +0100

    use kotlin.uuid

--- shared/src/commonMain/kotlin/com/shakeyourtown/missions/models/Organizer.kt
+++ shared/src/commonMain/kotlin/com/shakeyourtown/missions/models/Organizer.kt
@@ -1,11 +1,12 @@
 package com.shakeyourtown.missions.models
 
 import kotlinx.serialization.Serializable
-import java.util.UUID
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @Serializable
-data class Organizer(
-    val id: String = UUID.randomUUID().toString(),
+data class Organizer @OptIn(ExperimentalUuidApi::class) constructor(
+    val id: String = Uuid.random().toString(),
     val username: String,
     val passwordHash: String,
     val name: String,
